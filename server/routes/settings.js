bd86757dae456793c7bb7389f3aab36f3af28c6f
shahensha shaik <152256113+shahensh@users.noreply.github.com>
2025-09-10 22:16:58 +0530

    Delete server directory

--- server/routes/settings.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Placeholder route
-router.get('/', (req, res) => {
-  res.json({ message: 'Settings route working' });
-});
-
-module.exports = router; 
